63e9d18d698cfd19220dd39d21f9f6630d33f555
Sujith M <sujithmurugeshan569@gmail.com>
2026-07-04 22:32:08 +0530

    Fix SSL certificate issue with Gmail SMTP connection

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ const emailTransporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Twilio client configuration
